d98838fb4234132f5b74d607a8e9e228d17674d9
Simran kumari <simrankumari.nea10b@gmail.com>
2024-07-16 00:37:41 +0530

    Update Print_all_Prime.cpp

--- Print_all_Prime.cpp
+++ Print_all_Prime.cpp
@@ -1,20 +1,40 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n, i , a ,b ;
-    cin >> a >> b;
-    for ( int n=a; n<= b; n++){
-        int i ;
-        for ( i= 2; i<= n; i++){
-            if ( n % i == 0){
-            break;
-            }
-        }
- 
- if ( i == n){
-    cout << n << "  " ;
- }
-    }   
- return 0 ;
+int main() {
+	int N,i,number;
+	cin >> N;
+	for(number=2; number<=N; number++){
+		for(i=2;i<=number ; i++){
+		if( number%i== 0){
+			break;
+		}
+	}
+	if(i==number){
+		cout << number << endl ;
+	}
+	}
+	
+	return 0;
 }
+
+                //y code 2 number k bich prime print krne k liye h
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n, i , a ,b ;
+//     cin >> a >> b;
+//     for ( int n=a; n<= b; n++){
+//         int i ;
+//         for ( i= 2; i<= n; i++){
+//             if ( n % i == 0){
+//             break;
+//             }
+//         }
+ 
+//  if ( i == n){
+//     cout << n << "  " ;
+//  }
+//     }   
+//  return 0 ;
+// }
     
